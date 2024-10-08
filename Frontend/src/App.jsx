b7efdfe31278646e74791372fe75cbfd299fc7ff
dinesh <arekardinesh685@gmail.com>
2024-10-08 23:43:01 +0530

    modify

--- Frontend/src/App.jsx
+++ Frontend/src/App.jsx
@@ -14,7 +14,6 @@ function App() {
         <Route path="/" element={<Home />} />
         <Route path="/create" element={<Create />} />
         <Route path="/edit/:id" element={<Edit />} />
-        <Route path="/read/:id" element={<Read />} />
       </Routes>
     </BrowserRouter>
   )
